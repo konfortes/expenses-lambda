9fa09b09520abf552bfb9dd09ba4a6454bb35de4
konfortes <ronen.konfortes@gettitan.io>
2019-03-18 09:08:05 +0200

    invoke local

--- lambda/handler.js
+++ lambda/handler.js
@@ -7,16 +7,12 @@ const xlsParser = require("./xls-parser")
 
 module.exports.parse = async (event, context) => {
   const s3Record = event.Records[0].s3
-  const Bucket = s3Record.bucket.name
-  const Key = s3Record.object.key
-
   const params = {
-    Bucket,
-    Key
+    Bucket: s3Record.bucket.name,
+    Key: s3Record.object.key
   }
 
   const response = await S3.getObject(params).promise()
   const parsed = xlsParser(response.Body)
   console.log("PARSED: " + JSON.stringify(parsed, null, 4))
-  // const parsed = XLSX.read(response.Body)
 }
