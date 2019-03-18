b56210a6d35571970916e501fc94bcc28ba8549d
konfortes <ronen.konfortes@gettitan.io>
2019-03-18 20:04:00 +0200

    fixed some configuration

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,12 +1,14 @@
 "use strict"
 
-// const XLSX = require('xlsx');
 const AWS = require("aws-sdk")
-const S3 = new AWS.S3()
+const s3Endpoint = process.env.S3_ENDPOINT
+const s3Config = s3Endpoint ? {
+  endpoint: s3Endpoint
+} : {}
+const S3 = new AWS.S3(s3Config)
 const xlsParser = require("./xls-parser")
 
 module.exports.parse = async (event, context) => {
-  console.log('EVENT: ', event);
   const s3Record = event.Records[0].s3
   const params = {
     Bucket: s3Record.bucket.name,
