e5165a321dc37c046622a66b0ac748a70a4a1724
konfortes <ronen.konfortes@gettitan.io>
2019-03-20 23:26:29 +0200

    save to elastic

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,11 +1,10 @@
-"use strict"
-
 const AWS = require("aws-sdk")
 const s3Config = getConfig()
-console.log(JSON.stringify(s3Config, null, 4));
-
 const S3 = new AWS.S3(s3Config)
 const xlsParser = require("./xls-parser")
+const {
+  Client
+} = require('@elastic/elasticsearch')
 
 module.exports.parse = async (event, context) => {
   const s3Record = event.Records[0].s3
@@ -18,22 +17,25 @@ module.exports.parse = async (event, context) => {
   const parsed = xlsParser(response.Body)
   console.log("PARSED: " + JSON.stringify(parsed, null, 4))
 
-  // const {
-  //   Client
-  // } = require('@elastic/elasticsearch@6')
-  // const client = new Client({
-  //   node: 'http://localhost:9200'
-  // })
 
-  // for (const expense of parsed) {
-  //   await client.index({
-  //     index: 'expenses',
-  //     id: `${expense.date}:${expense.name}`,
-  //     body: {
-  //       ...expense
-  //     }
-  //   })
-  // }
+  const elasticClient = new Client({
+    node: 'http://localhost:9200'
+  })
+
+  for (const expense of parsed) {
+    try {
+      await elasticClient.index({
+        index: 'expenses',
+        id: `${expense.date}:${expense.name}`,
+        body: {
+          ...expense,
+          debitAmount: transformAmount(expense.debitAmount)
+        }
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
 
 function getConfig() {
@@ -43,4 +45,8 @@ function getConfig() {
     sslEnabled: process.env.SSL_ENABLED,
     s3ForcePathStyle: process.env.S3_FORCE_PATH_STYLE
   }
+}
+
+function transformAmount(amountText) {
+  return parseInt(amountText.replace(/[^\d.-]/g, ''))
 }
